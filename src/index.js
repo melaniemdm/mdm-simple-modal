fe322892d79d796fb96897da38a7aa3d0300bf76
melaniemdm <melanieborja.pro@gmail.com>
2023-02-23 18:00:41 +0100

    fix(js & css) props style

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react'
 import './style.css';
+const close = new URL("../assets/close.png", import.meta.url)
+import React from 'react';
 
 export function openModal(){
-const id= document.querySelector(".modalContainer")
+const id= document.querySelector(".backgroundModal")
 id.style.display="flex"
 }
 function closeModal(){
-    const id= document.querySelector(".modalContainer")
+    const id= document.querySelector(".backgroundModal")
 id.style.display="none"
 }
 
 
 
 export function ModalEmployeeCreated(props){
-   
-    return <div  className="modalContainer">
+    const width = props.width? props.width:'500px';
+    return <div className='backgroundModal'> <div  className="modalContainer" style={{width: width, backgroundColor: 'white', height: '50px'}}> 
         <div  className="modal">{props.text}</div>
         <div className="container-close">
 <div className='close' onClick={closeModal}> 
-     x
-      
+    <img src={close} alt='close'></img>      
 </div></div>
               
-    </div>
+    </div></div>
 }
