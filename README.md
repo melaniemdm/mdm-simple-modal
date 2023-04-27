d3c71ee4331d2874c22f7e2d11bd99a1a6d85d45
melaniemdm <melanieborja.pro@gmail.com>
2023-04-27 14:51:03 +0200

    documentation  refacto(js)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mdm-simple-modal
 
-> Made with create-react-library
+> react component of a modal to display text
 
 [![NPM](https://img.shields.io/npm/v/simple-modal.svg)](https://www.npmjs.com/package/simple-modal) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css'
-const close = new URL('../assets/close.png', import.meta.url)
 import React from 'react'
+const close = new URL('../assets/close.png', import.meta.url)
 
 export function openModal() {
   const id = document.querySelector('.backgroundModal')
@@ -8,41 +8,47 @@ export function openModal() {
 }
 function closeModal() {
   const id = document.querySelector('.backgroundModal')
-  id.style.display = 'none';
-  console.log("close modal")
+  id.style.display = 'none'
+  console.log('close modal')
   console.log(id)
 }
 
 export function SimpleModal(props) {
-  const width = props.options.width ? props.options.width : '500px';
-  const bgColor = props.options.bgColor ? props.options.bgColor : 'white';
-  const height = props.options.height ? props.options.height : '50px';
-  const maskBgColor = props.options.maskBgColor? props.options.maskBgColor : 'gray';
-  const maskBgOpacity = props.options.maskBgOpacity? props.options.maskBgOpacity : '1';
-  const imgUrl = props.options.imgUrl ? props.options.imgUrl : "";
-  const zIndex = props.options.zIndex ? props.options.zIndex : "100";
-  
-  return (
-    <div className='backgroundModal' style={{backgroundColor:maskBgColor, opacity: maskBgOpacity, zIndex:zIndex}}>
-      {' '}
-       <div className='containerC-close' style={{ width: width}}>
-          <div className='close' onClick={closeModal}>
-            <img src={close} alt='close'></img>
-          </div>
-        </div>
+  const width = props.options.width ? props.options.width : '500px'
+  const bgColor = props.options.bgColor ? props.options.bgColor : 'white'
+  const height = props.options.height ? props.options.height : '50px'
+  const maskBgColor = props.options.maskBgColor
+    ? props.options.maskBgColor
+    : 'gray'
+  const maskBgOpacity = props.options.maskBgOpacity
+    ? props.options.maskBgOpacity
+    : '1'
+  const imgUrl = props.options.imgUrl ? props.options.imgUrl : ''
+  const zIndex = props.options.zIndex ? props.options.zIndex : '100'
 
+  return (
+    <div
+      className='backgroundModal'
+      style={{
+        backgroundColor: maskBgColor,
+        opacity: maskBgOpacity,
+        zIndex: zIndex
+      }}
+    >
+      {' '}
+      <div className='containerC-close' style={{ width: width }}>
+        <div className='close' onClick={closeModal}>
+          <img src={close} alt='close' />
+        </div>
+      </div>
       <div
         className='modalContainer'
         style={{ width: width, backgroundColor: bgColor, height: height }}
       >
-       
-
         {/* texte modal */}
         <div className='modal'>{props.text}</div>
         {/* image modal  */}
-        <img src={imgUrl} alt="image" ></img>
-
-        
+        <img src={imgUrl} alt='image' />
       </div>
     </div>
   )
